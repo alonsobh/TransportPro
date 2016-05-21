974bf491cd6e6b665fbd57ca822042903d3b4c57
Alon <Alon@ALON>
2016-05-21 09:43:14 -0500

    Add project files.

---
+++ TransportPro.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TransportPro.Web.Global" Language="C#" %>
---
+++ TransportPro.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TransportPro.Web.Startup))]
+namespace TransportPro.Web
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
